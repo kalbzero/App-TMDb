95582bce0c23a2f07cc198e837f0ed43406c65ca
kalbzero <edward.ramos.guimaraes@gmail.com>
2020-08-15 17:21:02 -0300

    add route to movie/:id and get the infos

--- src/app/top100/top100.component.ts
+++ src/app/top100/top100.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ApiTmdbService } from '../shared/services/api-tmdb.service';
 
@@ -15,12 +16,18 @@ export class Top100Component implements OnInit {
   movies: any[];
 
   constructor(
-    private apiTmdbService: ApiTmdbService
+    private apiTmdbService: ApiTmdbService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.movies = this.apiTmdbService.getTopRated();
-    console.log(this.movies);
   }
 
+  /**
+   * Open the Movie Page Infos 
+   */
+  openMovie(movieId: number) {
+    this.router.navigate(['movie/' + movieId]);
+  }
 }
